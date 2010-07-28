5bd0343ab5cf0e60b9596562a73cb1798ae3ae5e
Steve Shreeve <steve.shreeve@gmail.com>
2010-07-28 10:27:47 -0400

    correct the HTTP status code range

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -588,7 +588,7 @@ module Sinatra
         end
       when res.respond_to?(:each)
         @response.body = res
-      when (100...599) === res
+      when (100..599) === res
         @response.status = res
       end
 
